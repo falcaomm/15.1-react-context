90173c4e6e14782bae745650d7694cfcb128c7b6
falcaomm <manu.m.falcao@gmail.com>
2023-02-08 16:24:48 -0300

    ao vivo context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Router from "./routes/Router";
+import { GlobalState } from "./contexts/GLobalState";
 
 export default function App() {
+
   return (
-    <>
+    <GlobalState>
       <Router />
-    </>
+    </GlobalState>
   );
 }
